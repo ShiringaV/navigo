a77460f7064da8c517623678e7e65e6b3162eaf9
VladMalakeev <33699922+VladMalakeev@users.noreply.github.com>
2018-01-08 22:42:21 +0200

    Добавлен запрос к серверу и обработка json object

--- App/app/src/main/java/navigo/app/MainActivity.java
+++ App/app/src/main/java/navigo/app/MainActivity.java
@@ -1,7 +1,6 @@
 package navigo.app;
 
-import android.content.Intent;
-import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,27 +11,26 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+
     final String LOG = "myLog";   //константа для логов
     TextView logo;
-    String city;                  //название города
+    String cityName = "odessa";                  //название города
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logo = (TextView) findViewById(R.id.logo);
-
 
         //TODO определяем координаты с LocationSingleton
 
@@ -47,27 +45,48 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(this);
 
         //адрес куда отправляем запрос + название опредилившегося города
-        String url = "http://navigo.ga/data.php?city=" + city;
-
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
+        String url = "http://navigo.zzz.com.ua/index.php?city=" + cityName;
+      //  String url ="https://www.foodapp.gq/android/data.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
-            public void onResponse(JSONObject response) {
-                logo.setText("Response => " + response.toString());
-                Log.d(LOG, response.toString());
+            public void onResponse(String response) {
+                JSONObject jsonObject;
+                String[] array_result = response.split("end_json_string");
+                String result = array_result[0];
+                Log.d(LOG, result.toString());
                 findViewById(R.id.progressBar).setVisibility(View.GONE);
-
-                /* извлекаем двнные из json строки
+                //извлекаем двнные из json строки
                try{
-                   JSONArray name = response.getJSONArray("name");
+                   //TODO распарсить JSON код в массив
+                   jsonObject = new JSONObject(result);
+                   JSONArray city = jsonObject.getJSONArray(cityName);
+                   for(int i = 0; i < city.length();i++ ){
+
+                       //Следущие дынные будут писаться в базу SQLite
+                       String name = city.getJSONObject(i).getString("name");               //название достопримечательности
+                       String type = city.getJSONObject(i).getString("type");               //тип достопримечательности
+                       String lat = city.getJSONObject(i).getString("lat");                 //широта
+                       String lon = city.getJSONObject(i).getString("lon");                 //долгота
+                       String descript = city.getJSONObject(i).getString("descript");       //описание
+                       String image = city.getJSONObject(i).getString("image");             //путь к картинке
+                       String video = city.getJSONObject(i).getString("video");             //путь к видео
+                       String sount_people = city.getJSONObject(i).getString("count_people"); //количество проголосовавших людей
+                       String summ_mark = city.getJSONObject(i).getString("summ_mark");     //общая сумма голосов
+                       String comment = city.getJSONObject(i).getString("comment");         //комментарии
+
+                       Log.d(LOG, "Название: " + name +
+                                  "Тип: " + type +
+                                  "Широта: " + lat +
+                                  "Долгота:" + lon +
+                                  "Описание: " + descript);
+
+                       //TODO заполняем таблицу данными из полученного массива
+                   }
                }
                catch (JSONException e){
                    logo.setText("Ошибка");
                    Log.d(LOG, e.toString());
                }
-               */
-
-
             }
         }, new Response.ErrorListener() {
 
@@ -79,16 +98,16 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-        queue.add(jsObjRequest);
+        queue.add(stringRequest);
 
 
 
-        //TODO распарсить JSON код в массив
+
 
         //TODO Создать SQL таблицы (если это первый запуск)
         //только после того как получим название города что назвать таблицу (data_odessa)
 
-        //TODO заполняем таблицу данными из полученного массива
+
 
         //TODO стественно делаем на всё проверки и если всё ок переходим intend-ом в следующие активити с картой
 
@@ -103,10 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
      */
 
-<<<<<<< HEAD
-  
-=======
->>>>>>> VladMalakeev
+
 
     }
 }
